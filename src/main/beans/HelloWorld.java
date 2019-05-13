b3582e657c1f4ded5af949612a3323c17637c9e0
Axios International Developer <info@axiosonline.com>
2019-05-12 17:44:31 -0700

    bean change

--- src/main/beans/HelloWorld.java
+++ src/main/beans/HelloWorld.java
@@ -6,12 +6,13 @@ import javax.enterprise.context.RequestScoped;
 @ManagedBean(name = "helloWorld", eager = true)
 @RequestScoped
 public class HelloWorld {
-    
+   
+   public static String helloMessage = "HCD - Welcome to JSF 2.x ";
    public HelloWorld() {
-      System.out.println("*** Please work!");
+      System.out.println(helloMessage);
    }
 	
    public String getMessage() {
-      return "*** Please work!!";
+      return helloMessage;
    }
 }
